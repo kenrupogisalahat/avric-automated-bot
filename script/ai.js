202961f704888fc93118eb660917a9b276f5e924
davidmpongo7 <mpongodavid723@gmail.com>
2025-02-08 16:27:41 +0100

    Update ai.js

--- script/ai.js
+++ script/ai.js
@@ -1,30 +1,35 @@
-const axios = require('axios');
-
 module.exports.config = {
-		name: 'ai',
-		version: '1.0.0',
-		role: 0,
-		hasPrefix: false,
-		description: "An AI command powered by OpenAI",
-		usages: "",
-		credits: 'Developer',
-		cooldown: 5,
+  name: `ai`,
+  version: "1.1.0",
+  permission: 0,
+  credits: "Metoushela",
+  description: "",
+  prefix: false,
+  premium: false,
+  category: "without prefix",
+  usage: ``,
+  cooldowns: 3,
+  dependency: {
+    "axios": ""
+  }
 };
 
-module.exports.run = async function({ api, event, args }) {
-		if (!args[0]) {
-				api.sendMessage("🌿--------✰---------🌿vas y🤒 entrer a ta question 🤓", event.threadID);
-				return;
-		}
+module.exports.run = async function ({api, event, args}) {
+  try{
+  const axios = require('axios');
+  let ask = args.join(' ');
+  if (!ask) {
+    return api.sendMessage('🌿 𝑫𝑨𝑽𝑩𝑶𝑻 n━━━━━━━━━━━\n\nplease provide a question.', event.threadID, event.messageID)
+  }
 
-		const question = args.join(" ");
-		const apiUrl = `https://openai-rest-api.vercel.app/hercai?ask=${encodeURIComponent(question)}&model=v3`;
-
-		try {
-				const response = await axios.get(apiUrl);
-				api.sendMessage(response.data.reply, event.threadID);
-		} catch (error) {
-				console.error("Error fetching response from OpenAI API:", error);
-				api.sendMessage("An error occurred while processing your request. Please try again later.", event.threadID);
-		}
-};
+  const res = await axios.get(`https://kaiz-apis.gleeze.com/api/gpt-4o?q=${ask}&uid=${event.senderID}`);
+  const reply = res.data.response;
+  if (res.error) {
+    return api.sendMessage('having some unexpected error while fetching api.', event.threadID, event.messageID)
+  } else {
+    return api.sendMessage(`🌿 𝑫𝑨𝑽𝑩𝑶𝑻 \n━━━━━━━━━━━\n\n${reply}`, event.threadID, event.messageID)
+  }
+  } catch (error) {
+    return api.sendMessage('having some unexpected error', event.threadID, event.messageID)
+  }
+                           }
