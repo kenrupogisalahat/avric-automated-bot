655529d6c33bc90d42698a97b6de9039e943fc0d
David Mpongo <mpongodavid723@gmail.com>
2025-02-16 11:18:02 +0100

    Update help.js

--- script/help.js
+++ script/help.js
@@ -1,102 +1,45 @@
-module.exports.config = {
-  name: 'help',
-  version: '1.0.0',
-  role: 0,
-  hasPrefix: true,
-  aliases: ['info'],
-  description: "Beginner's guide",
-  usage: "Help [page] or [command]",
-  credits: 'Develeoper',
-};
-module.exports.run = async function({
-  api,
-  event,
-  enableCommands,
-  args,
-  Utils,
-  prefix
-}) {
-  const input = args.join(' ');
-  try {
-    const eventCommands = enableCommands[1].handleEvent;
-    const commands = enableCommands[0].commands;
-    if (!input) {
-      const pages = 20;
-      let page = 1;
-      let start = (page - 1) * pages;
-      let end = start + pages;
-      let helpMessage = `  ✰🌿𝗗𝗔𝗩𝗕𝗢𝗧🌿✰\n  𝚌𝚖ｄ:\nAndroid Download 👉: 
+var alice = {
+ command: "help",
+ type: "auto",
+ author: "leiamnash",
+ restrict: "none",
+ cooldown: 1
+}
 
-iOS Download 👉: https://files.appsgeyser.com/Davbot%20App_18522058.apk🌿━━━━━━━━━━━━━━🌿\n\n`;
-      for (let i = start; i < Math.min(end, commands.length); i++) {
-        helpMessage += `🌿\t${i + 1}. ➳ ${prefix}${commands[i]} \n`;
-      }
-      helpMessage += '\n';
-      eventCommands.forEach((eventCommand, index) => {
-        helpMessage += `\t${index + 1}. 🦅🌿➳${prefix}${eventCommand} \n`;
-      });
-      helpMessage += `\n𝐏𝐀𝐆𝐄 ${page}/${Math.ceil(commands.length / pages)}. 𝗧𝗼 𝘃𝗶𝗲𝘄 𝘁𝗵𝗲 𝗡𝗲𝘅𝘁 𝗣𝗮𝗴𝗲, 𝗧𝘆𝗽𝗲🌿 '${prefix}𝗵𝗲𝗹𝗽 𝗽𝗮𝗴𝗲🎮 𝗡𝘂𝗺𝗯𝗲𝗿'. 𝗧𝗼 𝗩𝗶𝗲𝘄 𝗶𝗻𝗳𝗼𝗿𝗺𝗮𝘁𝗶𝗼𝗻🌿 𝗔𝗯𝗼𝘂𝘁 𝗮 𝗦𝗽𝗲𝗰𝗶𝗳𝗶𝗰 𝗖𝗼𝗺𝗺𝗮𝗻𝗱, 𝗧𝘆𝗽𝗲. '${prefix}𝗛𝗲𝗹𝗽 𝗖𝗼𝗺𝗺𝗮𝗻𝗱 𝗡𝗮𝗺𝗲🌿`;
-      api.sendMessage(helpMessage, event.threadID, event.messageID);
-    } else if (!isNaN(input)) {
-      const page = parseInt(input);
-      const pages = 20;
-      let start = (page - 1) * pages;
-      let end = start + pages;
-      let helpMessage = ` 🌿Android Download : 👉 https://files.appsgeyser.com/Davbot%20App_18522058.apk
+async function command({ alice, api, axios, bot, cache, chat, database, event, font, fs, language, log, message, path, scraper, wrapper }) {
+try {
+   var leiamV = Array.from(global.command.values());
+   var leiamX = fs.readdirSync(path.join(__dirname, "..", "commands/")).filter(leiam => leiam.endsWith(".js"));
+   let leiamZ = 10;
+   let leiamB = 1;
+  var leiamU = Math.ceil(leiamV.length / leiamZ);
+  var leiamE = parseInt(chat);
+   var leiamA = event.body.split(" ");
+  leiamA.splice(0, 1);
+  leiamA = leiamA.join(" ");
+if (!isNaN(leiamE) && leiamE >= 1 && leiamE <= leiamU) leiamB = leiamE;
+ var leiamC = []
+ var leiamD = "";
+ for (var leiam of leiamX) {
+ var leiamY = require(path.join(__dirname, "..", `commands/${leiam}`));
+   leiamC.push(leiamY.alice.command);
+}
+ var leiamF = (leiamB * leiamZ) - leiamZ;
+ var leiamG = leiamB * leiamZ;
+for (var leiamH = leiamF; leiamH < leiamG; leiamH++) {
+  if (leiamC[leiamH] != undefined) {
+var leiamI = alice.prefix + leiamC[leiamH] + "\n";
+    leiamD += leiamI;
+  }
+}
+return message(await font(language.help.replace("{0}", leiamD).replace("{1}", `${leiamB}/${leiamU}`)), "📑");
+} catch(error) {
+ log.error(`[ ${this.alice.command} ] » ${error}`);
+  return bot.chat(`[ ${this.alice.command} ] » ${language.error}`, event.threadID, event.messageID);
+ }
+}
 
-iOS Download 👉: https://files.appsgeyser.com/Davbot%20App_18522058.apk✰𝗗𝗔𝗩𝗕𝗢𝗧✰🌿\n 𝚌𝚖ｄ🌿\n━━━━━━━━━━━━━━━\n\n`;
-      for (let i = start; i < Math.min(end, commands.length); i++) {
-        helpMessage += `\t${i + 1}. 🌿➳ ${prefix}${commands[i]} \n`;
-      }
-      helpMessage += '\n';
-      eventCommands.forEach((eventCommand, index) => {
-        helpMessage += `\t${index + 1}🌿➳${prefix}${eventCommand} \n`;
-      });
-      helpMessage += `\n𝗣𝗮𝗴𝗲 ${page} 𝗢𝗳 ${Math.ceil(commands.length / pages)}\n𝗕𝘁𝘄 𝗖𝗿𝗲𝗮𝘁𝗼𝗿 𝘆𝗼𝘂𝗿 𝗼𝘄𝗻 𝗯𝗼𝘁➪➪ ♧ \n`;
-      api.sendMessage(helpMessage, event.threadID, event.messageID);
-    } else {
-      const command = [...Utils.handleEvent, ...Utils.commands].find(([key]) => key.includes(input?.toLowerCase()))?.[1];
-      if (command) {
-        const {
-          name,
-          version,
-          role,
-          aliases = [],
-          description,
-          usage,
-          credits,
-          cooldown,
-          hasPrefix
-        } = command;
-        const roleMessage = role !== undefined ? (role === 0 ? '➛ Permission: user' : (role === 1 ? '➛ Permission: admin' : (role === 2 ? '➛ Permission: thread Admin' : (role === 3 ? '➛ Permission: super Admin' : '')))) : '';
-        const aliasesMessage = aliases.length ? `➛ Aliases: ${aliases.join(', ')}\n` : '';
-        const descriptionMessage = description ? `Description: ${description}\n` : '';
-        const usageMessage = usage ? `➛ Usage: ${usage}\n` : '';
-        const creditsMessage = credits ? `➛ Credits: ${credits}\n` : '';
-        const versionMessage = version ? `➛ Version: ${version}\n` : '';
-        const cooldownMessage = cooldown ? `➛ Cooldown: ${cooldown} second(s)\n` : '';
-        const message = ` 「 Command 」\n\n➛ Name: ${name}\n${versionMessage}${roleMessage}\n${aliasesMessage}${descriptionMessage}${usageMessage}${creditsMessage}${cooldownMessage}`;
-        api.sendMessage(message, event.threadID, event.messageID);
-      } else {
-        api.sendMessage('Command not found.', event.threadID, event.messageID);
-      }
-    }
-  } catch (error) {
-    console.log(error);
-  }
-};
-module.exports.handleEvent = async function({
-  api,
-  event,
-  prefix
-}) {
-  const {
-    threadID,
-    messageID,
-    body
-  } = event;
-  const message = prefix ? '🌿➳𝖲𝗒𝗌𝗍𝖾𝗆 𝗉𝗋𝖾𝖿𝗂𝗑 𝗂𝗌:🎲\n🌿➳y𝗈𝗎𝗋 𝖼𝗁𝖺𝗍𝖻𝗈𝗍 𝗉𝗋𝖾𝖿𝗂𝗑 𝗂𝗌: ' + prefix : "🌿 𝗗𝗔𝗩𝗕𝗢𝗧 🌿\n━━━━━━━━━━━━━━━\nSorry i don't have prefix";
-  if (body?.toLowerCase().startsWith('prefix')) {
-    api.sendMessage(message, threadID, messageID);
-  }
-  }
+module["exports"] = {
+  alice,
+  command
+}
